c186fc7a43d2968877f5625e02c8039fe087806e
Andika Halim <andika.535240096@stu.untar.ac.id>
2025-11-24 12:29:46 +0700

    Database sqlite dengan prisma

---
+++ app/mahasiswa/page.tsx
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function UsersPage() {
+  const data = await prisma.mahasiswa.findMany(); 
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold mb-4">Daftar Data dari SQLite</h1>
+      <ul>
+        {data.map((item: any) => (
+          <li key={item.id} className="mb-2 border-b pb-2">
+            {/* Sesuaikan field di bawah dengan schema anda (misal item.nama, item.email) */}
+            Nama: {item.nama} 
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
